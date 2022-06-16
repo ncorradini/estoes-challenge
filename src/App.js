5f3283bae85593d99c533feaf8e9a19c1116cf2d
Nicolás Corradini <nicocorradini471@gmail.com>
2022-06-16 10:01:44 -0300

    Implemented layout and routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import { Box } from '@mui/material';
+import { Route, Routes } from 'react-router-dom';
+import Layout from './Layout';
+import TableProyects from './components/TableProyects';
+import Form from './components/Form';
 
 const App = () => {
   return (
-    <Box>
-      Init proyect
-    </Box>
+    <Layout>
+      {location.pathname === '/' && <TableProyects />}
+      <Routes>
+        <Route path="/create" element={<Form />} />
+      </Routes>
+    </Layout>
   );
 };
 
